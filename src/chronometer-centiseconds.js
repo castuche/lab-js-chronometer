8195d4e210a59f8819c87966723220a10afd254a
Catherine Fournier <fourniercath@hotmail.com>
2023-11-28 17:36:33 +0100

    finished lab with bonus

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,47 @@
 class Chronometer {
-  constructor() {
-    // ... your code goes here
-  }
+    constructor() {
+    this.currentTime=0;
+    this.intervalId=null;}
 
-  start(printTimeCallback) {
-    // ... your code goes here
-  }
+    start(printTimeCallback) {
+      this.intervalId = setInterval(()=>{
+        this.currentTime++;
+        if (typeof printTimeCallback === "function")
+        {printTimeCallback();}} ,10)
+      }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime/6000);
   }
-
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 100 % 60 );
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    return this.currentTime % 100;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let stringValue = `${value}`;
+    if (stringValue.length<2){
+      stringValue="0"+stringValue;
+    }
+    return stringValue;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime=0;
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    let centiseconds = this.computeTwoDigitNumber(this.getCentiseconds());
+
+    return `${minutes}:${seconds}.${centiseconds}`; 
   }
 }
